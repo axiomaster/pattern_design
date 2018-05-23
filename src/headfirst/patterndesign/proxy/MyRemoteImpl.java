62d3ad4cb8afad91a0d55de42f00d66e02cc891d
axiomaster <jiangshanliuyun@gmail.com>
2018-05-23 23:18:52 +0800

    代理模式

---
+++ src/headfirst/patterndesign/proxy/MyRemoteImpl.java
@@ -0,0 +1,27 @@
+//package headfirst.patterndesign.proxy;
+
+import java.rmi.Naming;
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+
+public class MyRemoteImpl extends UnicastRemoteObject implements MyRemote {
+
+    public MyRemoteImpl() throws RemoteException {
+
+    }
+
+    @Override
+    public String sayHello() throws RemoteException {
+        return "Server say, 'hi'";
+    }
+
+    public static void main(String[] args) {
+        try {
+            MyRemote service = new MyRemoteImpl();
+            Naming.rebind("RemoteHello", service);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
